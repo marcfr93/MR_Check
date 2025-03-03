2e08bdd56791a9b93a28b65a80dbaeb4914e017d
marcfr93 <marcferrater@gmail.com>
2025-03-03 16:29:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from utils.process_mr import process_mr
+from utils.process_mr import process_mr
 #from utils.pre_process import pre_process
 #from utils.process_extmytime import process_extmytime
 
@@ -10,8 +10,11 @@ st.set_page_config(layout="wide")
 monthly_reports = st.file_uploader("Upload Monthly Reports", accept_multiple_files=True)
 
 if monthly_reports:
-    hours_extmytime = st.file_uploader("Upload hours")
+    file_extmytime = st.file_uploader("Upload hours")
     if hours_extmytime:
+        hours_extmytime = process_extmytime(file_extmytime)
+        process_mr(monthly_reports, hours_extmytime)
         for report in monthly_reports:
             st.write(f"Filename {report.name}")
+            
 
