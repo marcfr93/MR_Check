1230d94a8d843a8118cfa0a166290a26fe15dd44
marcfr93 <marcferrater@gmail.com>
2025-03-03 13:53:40 +0100

    Create MR_process.py

---
+++ utils/MR_process.py
@@ -0,0 +1,11 @@
+import os
+import re
+from datetime import datetime
+from pathlib import Path
+import warnings
+
+import docx2txt
+import pandas as pd
+import math
+from unidecode import unidecode
+import time
