bf46171c2c3a75c24d9d5c7a92ed8884de17b4b7
marcfr93 <marcferrater@gmail.com>
2025-03-03 14:52:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 # Select your folder with MR
-text = st.text_area("Select the folder with the Monthly Reports to be processed")
 monthly_reports = st.file_uploader("Upload Monthly Reports", accept_multiple_files=True)
 
 if monthly_reports:
-    text = st.text_area("Select the XLSX file with the hours from ExtMyTime")
     hours_extmytime = st.file_uploader("Upload hours")
     if hours_extmytime:
         check_mr()
