c840aed9e1e7a639fe0fa5f98fa59a115eff6f69
marcfr93 <marcferrater@gmail.com>
2025-03-11 12:15:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,32 +18,33 @@ if st.button("Clear Output"):
     st.session_state.output_text = ""
     st.session_state.previous_name = ""
     st.session_state.processed = False
-    st.rerun()
 
 # Select your folder with MR
-monthly_reports = st.file_uploader("Upload Monthly Reports", accept_multiple_files=True)
+monthly_reports = st.file_uploader("Upload Monthly Reports", accept_multiple_files=True, key="mr_uploader")
 
+# Check if new files are uploaded and clear the session state accordingly
 if monthly_reports:
-    file_extmytime = st.file_uploader("Upload hours")
-    if file_extmytime and not st.session_state.processed:
-        # Reset output before processing new files
-        st.session_state.output_text = ""
-        st.session_state.previous_name = ""
-        
-        # Process the files
-        results = process_mr(monthly_reports, file_extmytime)
-        
-        for index, row in results.iterrows():
-            if st.session_state.previous_name != row["Name"]:
-                st.session_state.previous_name = row["Name"]
-                st.session_state.output_text += f"\n**Contract {row['Reference']}, {row['Name']}** \n"
-                
-            st.session_state.output_text += f"- {row['Error']} \n"
-        
-        # Mark as processed to prevent reprocessing on rerun
-        st.session_state.processed = True
+    file_extmytime = st.file_uploader("Upload hours", key="time_uploader")
+    if file_extmytime:
+        # Check if files have changed by comparing with stored session state
+        if not st.session_state.processed or st.session_state.previous_name != monthly_reports[0].name:
+            # Reset session state before processing
+            st.session_state.output_text = ""
+            st.session_state.previous_name = monthly_reports[0].name
+            st.session_state.processed = False
+            
+            # Process the files
+            results = process_mr(monthly_reports, file_extmytime)
+            for index, row in results.iterrows():
+                if st.session_state.previous_name != row["Name"]:
+                    st.session_state.previous_name = row["Name"]
+                    st.session_state.output_text += f"\n**Contract {row['Reference']}, {row['Name']}** \n"
+                    
+                st.session_state.output_text += f"- {row['Error']} \n"
+            
+            # Mark as processed to prevent reprocessing on rerun
+            st.session_state.processed = True
 
     # Display the output
     st.markdown(st.session_state.output_text)
-            
 
