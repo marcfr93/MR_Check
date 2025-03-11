1612999456d1b12a59cd14af943907ce731d01c7
marcfr93 <marcferrater@gmail.com>
2025-03-11 12:33:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,25 +6,31 @@ import pandas as pd
 # Configure layout of page, must be first streamlit call in script
 st.set_page_config(layout="wide")
 
+# Clear the session state to ensure fresh output each time
+if 'output_text' in st.session_state:
+    del st.session_state['output_text']
+if 'previous_name' in st.session_state:
+    del st.session_state['previous_name']
+
 # Select your folder with MR
 monthly_reports = st.file_uploader("Upload Monthly Reports", accept_multiple_files=True)
 
 if monthly_reports:
+    # Clear the session state to ensure fresh output each time
+    st.session_state.output_text = ""
+    st.session_state.previous_name = ""
+
     file_extmytime = st.file_uploader("Upload hours")
     if file_extmytime:
         results = process_mr(monthly_reports, file_extmytime)
-        # Clear the session state to ensure fresh output each time
-        st.session_state.output_text = ""
-        st.session_state.previous_name = ""
         #previous_name = ""
         #text = ""
-        widget = st.empty()
         for index, row in results.iterrows():
             if st.session_state.previous_name != row["Name"]:
                 st.session_state.previous_name = row["Name"]
                 st.session_state.output_text += f"\n**Contract {row['Reference']}, {row['Name']}** \n"
                 
             st.session_state.output_text += f"- {row['Error']} \n"
-        widget.write(str(st.session_state.output_text))    
-        #st.markdown(st.session_state.output_text)
+            
+        st.markdown(st.session_state.output_text)
 
