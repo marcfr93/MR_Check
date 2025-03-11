f6fc487c1a162d8b140ed8b3abbdb0fecd1e024c
marcfr93 <marcferrater@gmail.com>
2025-03-11 11:48:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 from utils.process_mr import process_mr
 import pandas as pd
 
-# Initialize session state variables if nor already initalized
-st.session_state.output_text = ""
-st.session_state.previous_name = ""
 
 # Configure layout of page, must be first streamlit call in script
 st.set_page_config(layout="wide")
@@ -13,6 +10,10 @@ st.set_page_config(layout="wide")
 monthly_reports = st.file_uploader("Upload Monthly Reports", accept_multiple_files=True)
 
 if monthly_reports:
+    # Clear the session state to ensure fresh output each time
+    st.session_state.output_text = ""
+    st.session_state.previous_name = ""
+
     file_extmytime = st.file_uploader("Upload hours")
     if file_extmytime:
         results = process_mr(monthly_reports, file_extmytime)
@@ -21,9 +22,9 @@ if monthly_reports:
         for index, row in results.iterrows():
             if st.session_state.previous_name != row["Name"]:
                 st.session_state.previous_name = row["Name"]
-                st.session_state.output_text = st.session_state.output_text + f"\n**Contract {row['Reference']}, {row['Name']}** \n"
+                st.session_state.output_text += f"\n**Contract {row['Reference']}, {row['Name']}** \n"
                 
-            st.session_state.output_text = st.session_state.output_text + f"- {row["Error"]} \n"
+            st.session_state.output_text += f"- {row['Error']} \n"
             
         st.markdown(st.session_state.output_text)
             
