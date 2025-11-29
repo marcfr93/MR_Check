301e2ca1e6f07dc632dad31e4e4aaed0877efc4b
marcfr93 <marcferrater@gmail.com>
2025-11-29 15:18:34 +0100

    Only consider new contract

--- app.py
+++ app.py
@@ -10,37 +10,22 @@ def main():
     st.cache_data.clear()
     st.cache_resource.clear()
 
-    tab1, tab2 = st.tabs(["Review 1159 Reports", "Review 1639 Reports"])
+    cont1 = st.container(border=True)
+    with cont1:
+        st.subheader("1. Upload Monthly Reports")
+        monthly_reports = st.file_uploader("Upload one or more monthly reports", accept_multiple_files=True, key="reports_1639")
 
-    with tab1:
-        cont1 = st.container(border=True)
-        with cont1:
-            st.subheader("1. Upload Monthly Reports")
-            monthly_reports = st.file_uploader("Upload one or more monthly reports", accept_multiple_files=True, key="reports")
+    cont2 = st.container(border=True)
+    with cont2:
+        st.subheader("2. Upload the hours from TimeTell")
+        file_timetell = st.file_uploader("Upload single file with the ExtMyTime hours", key="hours2")
 
+    if monthly_reports and file_timetell: #and file_extmytime:
         cont2 = st.container(border=True)
+        results = process_mr_1639(monthly_reports, file_timetell)
         with cont2:
-            st.subheader("2. Upload ExtMyTime hours")
-            file_extmytime = st.file_uploader("Upload single file with the ExtMyTime hours", key="hours")
-        if monthly_reports and file_extmytime:
-            cont3 = st.container(border=True)
-            results = process_mr(monthly_reports, file_extmytime)
-            with cont3:
-                st.subheader("3. Review Results")
-                st.dataframe(results, key="results_wdg")
-
-    with tab2:
-        cont1 = st.container(border=True)
-        with cont1:
-            st.subheader("1. Upload Monthly Reports")
-            monthly_reports = st.file_uploader("Upload one or more monthly reports", accept_multiple_files=True, key="reports_1639")
-
-        if monthly_reports: #and file_extmytime:
-            cont2 = st.container(border=True)
-            results = process_mr_1639(monthly_reports) #, file_extmytime)
-            with cont2:
-                st.subheader("2. Review Results")
-                st.dataframe(results, key="results_wdg_1639")
+            st.subheader("2. Review Results")
+            st.dataframe(results, key="results_wdg_1639")
 
 # Configure layout of page, must be first streamlit call in script
 st.set_page_config(layout="wide")
